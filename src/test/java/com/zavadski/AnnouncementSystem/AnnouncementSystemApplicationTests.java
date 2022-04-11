fa4df6df540473902ddfc460c1af564a7dc38e96
zavadskivadzim <kp002210@mail.ru>
2022-04-11 21:52:28 +0300

    create spring-boot app

---
+++ src/test/java/com/zavadski/AnnouncementSystem/AnnouncementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zavadski.AnnouncementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
