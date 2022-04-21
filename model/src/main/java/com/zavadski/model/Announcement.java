a8266beca8d79eb206d8020c0eb0c95e86b1aab2
Vadzim Zavadski <kp002210@mail.ru>
2022-04-21 18:06:30 +0300

    Announcement

--- model/src/main/java/com/zavadski/model/Announcement.java
+++ model/src/main/java/com/zavadski/model/Announcement.java
@@ -1,19 +1,52 @@
 package com.zavadski.model;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.zavadski.model.enumeration.Status;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Table(name = "announcement")
 public class Announcement {
 
     @Id
     @Column(name = "id")
     private UUID id = UUID.randomUUID();
+
+    @Column
+    private String body;
+
+    @Column
+    private Integer price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id")
+    private User user;
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+3")
+    private LocalDate dateOfCreating;
+
+    @Column(name = "created_at")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+3")
+    private LocalDate dateOfClosing;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
 }
